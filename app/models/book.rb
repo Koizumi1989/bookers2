ad076b9cb87385f05225905ddbb8b03487fca2dc
Koizumi1989 <08033431635a@gmail.com>
2022-02-26 01:46:25 +0000

    bookers2

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
 
 
+
   #book側からみるとユーザーが一人に決まるようになっている。
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
